2e3a9ac3ff9b244635080195b1889b336f69e7fe
shinudesu <arokidaaron93@gmail.com>
2021-06-12 18:10:30 +0800

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,1 @@
-module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['prettier', 'eslint-ruppy'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
-};
+module.exports = { extends: ['ruppy-node', 'ruppy-ts', 'ruppy-jest'] };
