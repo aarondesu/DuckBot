d086fce065ba01566e853b2c488ce2785d620736
shinudesu <arokidaaron93@gmail.com>
2021-06-18 22:51:08 +0800

    Refractored and fixed autotranslator listener

--- src/listeners/autotranslator.ts
+++ src/listeners/autotranslator.ts
@@ -1,10 +1,20 @@
 import { Listener } from 'discord-akairo';
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import Detectlanguage from 'detectlanguage';
 import axios, { AxiosRequestConfig } from 'axios';
 import logger from 'lib/logger';
 import { oneLine } from 'common-tags';
 
+type DeepLTranslate = {
+  data: {
+    translations: {
+      translatedText: string;
+    };
+  };
+};
+
+// TODO: better implementation for handling requests
+// TODO: better filtering
 export default class AutoTranslator extends Listener {
   // #region Variables
   languageApi: Detectlanguage;
@@ -22,70 +32,90 @@ export default class AutoTranslator extends Listener {
     );
   }
 
+  private async detectLanguage(content: string) {
+    try {
+      logger.info('Getting language of text...');
+      const result = await this.languageApi.detect(content);
+      const language = result.pop()?.language as string;
+
+      return [language, null];
+    } catch (error) {
+      return [null, error as string];
+    }
+  }
+
+  private static async translateText(language: string, toTranslate: string) {
+    try {
+      // Axios config
+      const option: AxiosRequestConfig = {
+        method: 'POST',
+        url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-key': process.env.RAPID_API_KEY as string,
+          'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+        },
+        data: {
+          q: toTranslate,
+          source: language.toUpperCase(),
+          target: 'EN',
+        },
+      };
+
+      logger.info('Requesting data from RAPID API/deepL... ');
+      // Request data
+      const response = await axios.request<DeepLTranslate>(option);
+      logger.info('Request reecieved from RAPID API/DeepL!');
+
+      return [response.data.data.translations.translatedText, null];
+    } catch (error) {
+      return [null, error as string];
+    }
+  }
+
   // TODO: stop intercepting messages when confronted with a command requiring additional info
   public async exec(message: Message) {
     // Check if message is not bot message, not a command, and is only from a server
     if (message.author.bot || !message.guild || message.content[0] === '?')
       return;
 
-    // Check language
-    this.languageApi
-      .detect(message.content)
-      .then((result) => {
-        // Get language code
-        const endResult = result.pop();
-        const language = endResult?.language as string;
+    // Detect language
+    const [language, langError] = await this.detectLanguage(message.content);
+    if (langError !== null) {
+      const strErr = oneLine`
+        Unable to process DetectLanguage request.
+        Reason: ${langError}
+      `;
 
-        // Check if language isn't EN
-        if (language?.toUpperCase() !== 'EN') {
-          // API request config
-          const option: AxiosRequestConfig = {
-            method: 'POST',
-            url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
-            headers: {
-              'content-type': 'application/json',
-              'x-rapidapi-key': process.env.RAPID_API_KEY as string,
-              'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
-            },
-            data: {
-              q: message.content,
-              source: language.toUpperCase(),
-              target: 'en',
-            },
-          };
+      logger.error(strErr);
+      await message.channel.send(strErr);
+    }
 
-          // Get API(deepL) response
-          logger.info('Sending data to RAPID API/deepL...');
-          axios
-            .request(option)
-            .then((response) => {
-              logger.info('Recieved response from RAPID API/DeepL');
+    // Check if language is not EN
+    if (language !== 'EN') {
+      // Request translated text
+      const [translatedText, transError] = await AutoTranslator.translateText(
+        language as string,
+        message.content
+      );
+      if (transError !== null) {
+        const strErr = oneLine`
+        Unable to process DeepL request.
+        Reason: ${langError}
+        `;
 
-              const data: string = response.data as string;
-              // const parsed: unknown = JSON.parse(data);
+        logger.error(strErr);
+        await message.channel.send(strErr);
+      }
 
-              // TODO: Parse Request and display proper info
-              return message.channel.send(data);
-            })
-            .catch((err) => {
-              logger.error(err as string);
-              return message.channel.send(
-                oneLine`
-                  Unable to process DeepL request.
-                  Reason: ${err as string}
-                `
-              );
-            });
-        }
-      })
-      .catch((err) => {
-        logger.error(err as string);
-        return message.channel.send(
-          oneLine`
-                  Unable to process DetectLanguage request.
-                  Reason: ${err as string}
-                `
-        );
-      });
+      // Create embed
+      const translatedEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Autotranslated text')
+        .setDescription(`${translatedText as string}`);
+
+      // Send translated message back to channel
+      await message.channel.send(translatedEmbed);
+    }
   }
 }
