90d596ba7ba0b0a1274c77c86a0766d81e895875
shinudesu <arokidaaron93@gmail.com>
2021-06-19 18:36:51 +0800

    Removed prefix reference

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,4 @@ import { Snowflake } from 'discord.js';
 export const ClientConfig = {
   owners: process.env.OWNER_IDS?.split(',') as Snowflake[],
   token: process.env.DISCORD_TOKEN,
-  defaultPrefix: '?',
 };
