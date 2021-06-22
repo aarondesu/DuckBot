ec87671e5a98ff7b8e85ae503236bf5249a2b0e5
shinudesu <arokidaaron93@gmail.com>
2021-06-22 18:50:17 +0800

    Added postgres database

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,14 @@
 /* eslint import/prefer-default-export: 0 */
 import { PresenceData, Snowflake } from 'discord.js';
+import dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config({ path: join(__dirname, '../.env') });
 
 export const ClientConfig = {
   owners: process.env.OWNER_IDS?.split(',') as Snowflake[],
   token: process.env.DISCORD_TOKEN,
+  prefix: process.env.PREFIX,
 };
 
 export const DuckPresence: PresenceData = {
@@ -14,4 +19,8 @@ export const DuckPresence: PresenceData = {
     },
   ],
   status: 'online',
+};
+
+export const DatabaseConfig = {
+  uri: process.env.DATABASE_URL,
 };
